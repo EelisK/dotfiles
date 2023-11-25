f4f509c30611fbf542bc7a932439ddb37a83f3f0
Eelis Kostiainen <eelis.kostiainen@gmail.com>
2023-11-26 00:33:20 +0200

    Update completions

--- dot_oh-my-zsh/custom/completion.zsh
+++ dot_oh-my-zsh/custom/completion.zsh
@@ -2,12 +2,12 @@
 ##          Completion                                        ##
 #==============================================================#
 
-command -v kubectl &> /dev/null && source <(kubectl completion zsh)
-command -v kubectl &> /dev/null && compdef kubecolor=kubectl
-command -v helm &> /dev/null && source <(helm completion zsh)
-command -v kind &> /dev/null && source <(kind completion zsh)
-command -v kubebuilder &> /dev/null && source <(kubebuilder completion zsh)
-command -v chezmoi &> /dev/null && source <(chezmoi completion zsh)
-command -v minikube &> /dev/null && source <(minikube completion zsh)
+command -v kubectl >/dev/null 2>&1 && source <(kubectl completion zsh)
+command -v kubectl >/dev/null 2>&1 && compdef kubecolor=kubectl
+command -v helm >/dev/null 2>&1 && source <(helm completion zsh)
+command -v kind >/dev/null 2>&1 && source <(kind completion zsh)
+command -v kubebuilder >/dev/null 2>&1 && source <(kubebuilder completion zsh)
+command -v chezmoi >/dev/null 2>&1 && source <(chezmoi completion zsh)
+command -v minikube >/dev/null 2>&1 && source <(minikube completion zsh)
 
-command -v rbenv &> /dev/null && eval "$(rbenv init - zsh)"
+command -v rbenv >/dev/null 2>&1 && eval "$(rbenv init - zsh)"
