e4406aa78fbd5cb44e228535570969b11fd93d7a
Eelis Kostiainen <eelis.kostiainen@gmail.com>
2023-11-27 00:47:29 +0200

    Install and configure stern

--- dot_oh-my-zsh/custom/completion.zsh
+++ dot_oh-my-zsh/custom/completion.zsh
@@ -9,5 +9,5 @@ command -v kind >/dev/null 2>&1 && source <(kind completion zsh)
 command -v kubebuilder >/dev/null 2>&1 && source <(kubebuilder completion zsh)
 command -v chezmoi >/dev/null 2>&1 && source <(chezmoi completion zsh)
 command -v minikube >/dev/null 2>&1 && source <(minikube completion zsh)
-
+command -v stern >/dev/null 2>&1 && source <(stern --completion zsh)
 command -v rbenv >/dev/null 2>&1 && eval "$(rbenv init - zsh)"
