333c089e868adaefb6f16047950ad4f3fe158102
Eelis Kostiainen <eelis@huuva.io>
2023-12-08 13:09:38 +0200

    Add markdown preview plugin

---
+++ dot_config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+---@type NvPluginSpec
+local M = {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+}
+
+return M
