96935444e8e847378e6061484a7d70463f150f25
Eelis Kostiainen <eelis@huuva.io>
2023-12-09 16:57:08 +0200

    Add figlet plugin

---
+++ dot_config/nvim/lua/exact_custom/plugins/figlet.lua
@@ -0,0 +1,11 @@
+---@type NvPluginSpec
+local M = {
+	"pavanbhat1999/figlet.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		-- comment support
+		"numToStr/Comment.nvim",
+	},
+}
+
+return M
