54ae6aeb0319965723d1e9f477f51f45ef2a539d
Eelis Kostiainen <eelis@huuva.io>
2024-03-14 20:02:20 +0200

    Configure wezterm ⚙

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,108 @@
+local wezterm = require("wezterm")
+local gpus = wezterm.gui.enumerate_gpus()
+
+local config = {
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 10,
+	-- font_rules = {
+	-- 	{
+	-- 		italic = true,
+	-- 		font = wezterm.font("Cica", { italic = true }),
+	-- 	},
+	-- 	{
+	-- 		italic = true,
+	-- 		intensity = "Bold",
+	-- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
+	-- 	},
+	-- },
+	check_for_updates = false,
+	use_ime = true,
+	ime_preedit_rendering = "Builtin",
+	use_dead_keys = false,
+	warn_about_missing_glyphs = false,
+	animation_fps = 1,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_rate = 0,
+	color_scheme = "Wombat",
+	color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
+	hide_tab_bar_if_only_one_tab = false,
+	adjust_window_size_when_changing_font_size = false,
+	selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 10,
+		bottom = 10,
+	},
+	colors = {},
+	exit_behavior = "CloseOnCleanExit",
+	window_close_confirmation = "AlwaysPrompt",
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 100,
+	enable_csi_u_key_encoding = true,
+	leader = { key = "Space", mods = "CTRL|SHIFT" },
+	key_tables = {},
+	mouse_bindings = {},
+	-- https://github.com/wez/wezterm/issues/2756
+	webgpu_preferred_adapter = gpus[1],
+	front_end = "OpenGL",
+}
+
+for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+	if gpu.backend == "Vulkan" and gpu.device_type == "IntegratedGpu" then
+		config.webgpu_preferred_adapter = gpu
+		config.front_end = "WebGpu"
+		break
+	end
+end
+
+config.hyperlink_rules = {
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		-- Before
+		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+		--format = '$0',
+		-- After
+		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
+}
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://github.com/$1/$3",
+})
+
+return config
