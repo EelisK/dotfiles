a49d1bb1000774bf16dce532a9a205a077aff030
Eelis Kostiainen <eelis@huuva.io>
2024-04-23 15:07:35 +0300

    Install operator-sdk

--- dot_oh-my-zsh/custom/completion.zsh
+++ dot_oh-my-zsh/custom/completion.zsh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC1090,SC2148
 #==============================================================#
 ##          Completion                                        ##
 #==============================================================#
@@ -7,11 +8,11 @@ command -v kubectl >/dev/null 2>&1 && compdef kubecolor=kubectl
 command -v helm >/dev/null 2>&1 && source <(helm completion zsh)
 command -v kind >/dev/null 2>&1 && source <(kind completion zsh)
 command -v kubebuilder >/dev/null 2>&1 && source <(kubebuilder completion zsh)
+command -v operator-sdk >/dev/null 2>&1 && source <(operator-sdk completion zsh)
 command -v chezmoi >/dev/null 2>&1 && source <(chezmoi completion zsh)
 command -v minikube >/dev/null 2>&1 && source <(minikube completion zsh)
 command -v stern >/dev/null 2>&1 && source <(stern --completion zsh)
 command -v rbenv >/dev/null 2>&1 && eval "$(rbenv init - zsh)"
-
 
 #==============================================================#
 ##          Custom completion                                 ##
