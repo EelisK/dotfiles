e581b558334de484006093710fc2b64040a4a713
Eelis Kostiainen <eelis@huuva.io>
2024-11-26 18:38:55 +0200

    Add key bindings

---
+++ dot_oh-my-zsh/custom/keybindings.zsh
@@ -0,0 +1,13 @@
+#==============================================================#
+##         Key bindings                                       ##
+#==============================================================#
+
+# ALT/Option + Left/right
+bindkey '^[[1;3C' forward-word
+bindkey '^[[1;3D' backward-word
+# ALT/Option + Shift + Right
+bindkey '^[[1;10C' end-of-line
+bindkey '^[[1;4C' end-of-line
+# ALT/Option + Shift + Left
+bindkey '^[[1;10D' beginning-of-line
+bindkey '^[[1;4D' beginning-of-line
