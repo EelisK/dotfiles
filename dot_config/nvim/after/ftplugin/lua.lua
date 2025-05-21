e5eaf3a06fcb09c7831365c74be3bc3b93d95052
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-05-22 00:23:42 +0300

    Configure java lsp

--- dot_config/nvim/after/ftplugin/lua.lua
+++ dot_config/nvim/after/ftplugin/lua.lua
@@ -12,5 +12,3 @@ vim.opt_local.expandtab = true
 vim.opt_local.tabstop = indent
 vim.opt_local.softtabstop = indent
 vim.opt_local.shiftwidth = indent
-vim.opt_local.colorcolumn = "120"
-vim.opt_local.textwidth = 119
