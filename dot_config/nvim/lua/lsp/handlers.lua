7afa4d89e59e5c849fc15a4f4aa94b40f3c63bd7
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-05-23 12:45:37 +0300

    Update handlers

--- dot_config/nvim/lua/lsp/handlers.lua
+++ dot_config/nvim/lua/lsp/handlers.lua
@@ -1,8 +1,16 @@
 local M = {}
 
+local m = vim.lsp.protocol.Methods
+
 ---@param event vim.api.keyset.create_autocmd.callback_args
 function M.on_attach(event)
   local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+  ---@param method string
+  ---@return boolean
+  local function supports(method)
+    return client:supports_method(method, event.buf)
+  end
+
   -- Create a shortcut for checkhealth
   vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { nargs = 0 })
 
@@ -26,19 +34,37 @@ function M.on_attach(event)
   }
 
   -- settings for the LSP framework
-  map("<leader>fm", vim.lsp.buf.format, "Format buffer")
-  map("gD", vim.lsp.buf.declaration, "Go to declaration")
-  map("gd", vim.lsp.buf.definition, "Go to definition")
-  map("gi", vim.lsp.buf.implementation, "Go to implementation")
-
-  map("<leader>sh", vim.lsp.buf.signature_help, "Show signature help")
-  map("K", vim.lsp.buf.hover, "Hover")
-
-  map("<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
-  map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
-
-  map("<leader>D", vim.lsp.buf.type_definition, "Go to type definition")
-  map("<leader>ra", require "lsp.actions.renamer", "Rename")
+  if supports(m.textDocument_formatting) then
+    map("<leader>fm", vim.lsp.buf.format, "Format buffer")
+  end
+  if supports(m.textDocument_declaration) then
+    map("gD", vim.lsp.buf.declaration, "Go to declaration")
+  end
+  if supports(m.textDocument_definition) then
+    map("gd", vim.lsp.buf.definition, "Go to definition")
+  end
+  if supports(m.textDocument_implementation) then
+    map("gi", vim.lsp.buf.implementation, "Go to implementation")
+  end
+  if supports(m.textDocument_references) then
+    map("gr", vim.lsp.buf.references, "Go to references")
+  end
+  if supports(m.textDocument_formatting) then
+    map("<leader>fm", vim.lsp.buf.format, "Format buffer")
+  end
+  if supports(m.textDocument_hover) then
+    map("K", vim.lsp.buf.hover, "Hover")
+  end
+  if supports(m.textDocument_typeDefinition) then
+    map("<leader>gt", vim.lsp.buf.type_definition, "Go to type definition")
+  end
+  if supports(m.textDocument_rename) then
+    map("<leader>ra", require "lsp.actions.renamer", "Rename")
+  end
+  -- Integration with the built-in completion
+  if supports(m.textDocument_completion) then
+    vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+  end
 
   -- diagnostic mappings
   map("<leader>lf", vim.diagnostic.open_float, "Open diagnostic float")
@@ -48,11 +74,10 @@ function M.on_attach(event)
   map("<leader>ln", function()
     vim.diagnostic.jump { count = 1, float = true }
   end, "Go to next diagnostic")
-  map("<leader>lq", vim.diagnostic.setloclist, "Open diagnostic Quickfix list")
 
   -- Symbol highlights
   -- Creates an autocmd to highlight the symbol under the cursor
-  if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+  if supports(m.textDocument_documentHighlight) then
     local lsp_hl_group = vim.api.nvim_create_augroup("eelisk.lsp.hl", { clear = false })
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       buffer = event.buf,
@@ -75,18 +100,9 @@ function M.on_attach(event)
     })
   end
 
-  -- Integration with the built-in completion
-  if client:supports_method "textDocument/completion" then
-    vim.lsp.completion.enable(true, client.id, event.buf, {
-      autotrigger = true,
-    })
-  end
-
   -- Auto-format ("lint") on save.
   -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-  if
-    not client:supports_method "textDocument/willSaveWaitUntil" and client:supports_method "textDocument/formatting"
-  then
+  if not client:supports_method(m.textDocument_willSaveWaitUntil) and client:supports_method(m.textDocument_formatting) then
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
       buffer = event.buf,
