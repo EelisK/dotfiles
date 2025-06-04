b6e734565cb4f52cbcb198996a16b0c8e82d85aa
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-06-04 21:36:28 +0300

    Update telescope keys

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,6 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   cmd = "Telescope",
   keys = {
-    -- telescope
     { "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" } },
     { "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" } },
     { "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" } },
@@ -14,19 +13,14 @@ return {
       "<cmd>Telescope current_buffer_fuzzy_find<CR>",
       { desc = "telescope find in current buffer" },
     },
-    { "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" } },
-    { "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" } },
-    { "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" } },
+    { "<leader>fc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" } },
+    { "<leader>fg", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" } },
+    { "<leader>ft", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" } },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" } },
     {
       "<leader>fa",
       "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
       { desc = "telescope find all files" },
-    },
-    {
-      "<leader>ft",
-      "<cmd>Telescope colorscheme<CR>",
-      { desc = "telescope find colorscheme", nowait = true },
     },
   },
   opts = {
