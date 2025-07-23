b76d57009187992346e5ce4b147e46a60e370c02
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-07-23 13:20:09 +0300

    Update blink.cmp config

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -10,57 +10,99 @@ M.dependencies = {
   "onsails/lspkind-nvim",
 }
 
+---@type blink.cmp.Config
 M.opts = {
   appearance = {
     -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- Adjusts spacing to ensure icons are aligned
     nerd_font_variant = "mono",
+    kind_icons = require("icons").lspkind,
   },
   signature = { enabled = true },
   keymap = {
-    preset = "enter",
+    preset = "default",
+    ["<Tab>"] = { "select_next", "fallback" },
+    ["<S-Tab>"] = { "select_prev", "fallback" },
+    ["<Up>"] = { "select_prev", "fallback" },
+    ["<Down>"] = { "select_next", "fallback" },
+    ["<C-p>"] = { "select_prev", "fallback" },
+    ["<C-n>"] = { "select_next", "fallback" },
+    ["<Enter>"] = { "accept", "fallback" },
 
-    ["<Tab>"] = {
-      "select_next",
-      "snippet_forward",
-      "fallback",
-    },
+    ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+    ["<C-d>"] = { "scroll_documentation_down", "fallback" },
 
-    ["<S-Tab>"] = {
-      "select_prev",
-      "snippet_backward",
-      "fallback",
+    ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+    ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+
+    ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+    ["<C-e>"] = { "hide", "fallback" },
+  },
+
+  cmdline = {
+    enabled = true,
+    sources = function()
+      local type = vim.fn.getcmdtype()
+      if type == "/" or type == "?" then
+        return { "buffer" }
+      end
+      if type == ":" then
+        return { "cmdline" }
+      end
+      return {}
+    end,
+    completion = {
+      menu = {
+        draw = {
+          columns = {
+            { "kind_icon", "label", gap = 1 },
+            { "kind" },
+          },
+        },
+      },
     },
   },
 
-  -- sources = {
-  --   providers = {
-  --     lsp = {
-  --       name = "LSP",
-  --       module = "blink.cmp.sources.lsp",
-  --       transform_items = function(_, items)
-  --         return vim.tbl_filter(function(item)
-  --           return item.kind ~= require("blink.cmp.types").CompletionItemKind.Keyword
-  --         end, items)
-  --       end,
-  --     },
-  --   },
-  -- },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lsp = {
+        min_keyword_length = 0,
+        score_offset = 0,
+      },
+      path = {
+        min_keyword_length = 0,
+      },
+      snippets = {
+        min_keyword_length = 2,
+      },
+      buffer = {
+        min_keyword_length = 5,
+        max_items = 5,
+      },
+    },
+  },
+
   completion = {
+    -- Disable auto brackets
+    -- NOTE: some LSPs may add auto brackets themselves anyway
+    accept = { auto_brackets = { enabled = false } },
 
     -- 'prefix' will fuzzy match on the text before the cursor
     -- 'full' will fuzzy match on the text before _and_ after the cursor
     -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
     keyword = { range = "full" },
 
-    -- Disable auto brackets
-    -- NOTE: some LSPs may add auto brackets themselves anyway
-    accept = { auto_brackets = { enabled = false } },
+    trigger = {
+      show_on_insert_on_trigger_character = true,
+      show_on_trigger_character = true,
+      show_on_keyword = true,
+    },
+
     documentation = {
       auto_show = true,
-      window = {
-        border = "rounded",
-      },
+      auto_show_delay_ms = 250,
+      treesitter_highlighting = true,
     },
 
     menu = {
