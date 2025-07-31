213a53091b50dd689be26ec0058b0e6ef638ad05
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-07-31 19:28:01 +0300

    Enable automatic imports for golang

--- dot_config/nvim/lsp/gopls.lua
+++ dot_config/nvim/lsp/gopls.lua
@@ -39,9 +39,37 @@ local function root_dir(bufnr, on_dir)
   end)
 end
 
+local function on_attach()
+  local autoimport = vim.api.nvim_create_augroup("eelisk/lsp/gopls/autoimport", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function(event)
+      local window = vim.api.nvim_get_current_win()
+      local params = vim.lsp.util.make_range_params(window, "utf-8")
+
+      ---@diagnostic disable-next-line: inject-field
+      params.context = { only = { "source.organizeImports" } }
+      local result = vim.lsp.buf_request_sync(event.buf, "textDocument/codeAction", params)
+      for cid, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        end
+      end
+    end,
+    group = autoimport,
+  })
+end
+
 ---@type vim.lsp.Config
 return {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = root_dir,
+  on_attach = on_attach,
+  on_exit = function()
+    vim.api.nvim_clear_autocmds { group = "eelisk/lsp/gopls/autoimport" }
+  end,
 }
