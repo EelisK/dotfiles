566ae6690b95ab5e6a250ae89623e850df5cfd5a
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2025-08-13 11:16:00 +0300

    Update mypy diagnostic configs

--- dot_config/nvim/lua/plugins/diagnostics/null-ls.lua
+++ dot_config/nvim/lua/plugins/diagnostics/null-ls.lua
@@ -19,7 +19,38 @@ M.config = function()
       diagnostics.terraform_validate,
       diagnostics.tfsec,
       -- python
-      diagnostics.mypy,
+      diagnostics.mypy.with {
+        args = function(params)
+          local opts = {
+            -- default options
+            "--hide-error-codes",
+            "--hide-error-context",
+            "--no-color-output",
+            "--show-absolute-path",
+            "--show-column-numbers",
+            "--show-error-codes",
+            "--no-error-summary",
+            "--shadow-file",
+            -- custom options
+            "--pretty",
+            "--strict",
+            "--warn-unused-configs",
+            "--warn-unused-ignores",
+            "--namespace-packages",
+            "--explicit-package-bases",
+          }
+          local config = params.cwd .. "/pyproject.toml"
+          ---@diagnostic disable-next-line: undefined-field
+          if vim.loop.fs_stat(config) then
+            table.insert(opts, "--config")
+            table.insert(opts, config)
+          end
+          table.insert(opts, params.bufname)
+          table.insert(opts, params.temp_path)
+          table.insert(opts, params.bufname)
+          return opts
+        end,
+      },
     },
   }
 
