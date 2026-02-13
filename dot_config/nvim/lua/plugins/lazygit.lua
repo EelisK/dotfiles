8ef38957cba6132948d205e88ef5a953ed545cfb
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2026-02-13 15:17:11 +0200

    Fix lazygit plugin <Esc> mapping

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -14,6 +14,9 @@ return {
   },
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    -- override the default terminal mode keybinding
+    -- to allow using <Esc> as configured in lazygit
+    { "<Esc>", "<Esc>", mode = "t" },
   },
   config = function()
     require("telescope").load_extension "lazygit"
