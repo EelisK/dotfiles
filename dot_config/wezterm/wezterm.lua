20cfeaa241059593a3044f7f52e89fc4d4259706
Eelis Kostiainen <eelis.kostiainen@aiven.io>
2026-03-27 15:29:07 +0200

    Disable native fullscreen mode on macos

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -22,6 +22,7 @@ local config = {
   -- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
   -- 	},
   -- },
+  native_macos_fullscreen_mode = false,
   send_composed_key_when_left_alt_is_pressed = true,
   send_composed_key_when_right_alt_is_pressed = true,
   check_for_updates = false,
